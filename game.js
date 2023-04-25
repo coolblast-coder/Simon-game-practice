ae4f0c85ed6e3cab3d237fed995cc8e7ae94c13f
Coolblast-coder <lyapunovantik@gmail.com>
2023-04-25 13:02:56 -0600

    added playSound, animatePress, button click detection with delay, keypress detection

--- game.js
+++ game.js
@@ -1,13 +1,55 @@
 var buttonColours = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
+var userClickedPattern = [];
 
 function nextSequence() {
-    var randomNumber = Math.floor(Math.random() * 4);
-    var randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(randomChosenColour); 
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100);  
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#" + randomChosenColour)
+    .fadeOut(100)
+    .fadeIn(100);
 }
 
+function playSound(key) {
+  switch (key) {
+    case "red":
+      var audio = new Audio("sounds/red.mp3");
+      audio.play();
+      break;
+    case "green":
+      var audio = new Audio("sounds/green.mp3");
+      audio.play();
+      break;
+    case "blue":
+      var audio = new Audio("sounds/blue.mp3");
+      audio.play();
+      break;
+    case "yellow":
+      var audio = new Audio("sounds/yellow.mp3");
+      audio.play();
+      break;
 
+    default:
+      break;
+  }
+}
 
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+  setTimeout(function(){
+    $("#" + currentColour).removeClass("pressed");
+  }, 200);
+}
 
+$(".btn").on("click", function (event) {
+  var userChosenColour = event.target.id;
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
+
+$("body").on("keypress", function(event) {
+    console.log(event.originalEvent.key);
+    
+})
